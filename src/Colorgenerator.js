a0e5c46cbe2606650537bd26fa9b5c45e9ba00bc
Mona Lisa Octocat <raniamet.90@gmail.com>
2023-05-03 23:24:44 +0200

    Final

--- src/Colorgenerator.js
+++ src/Colorgenerator.js
@@ -1,21 +1,30 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Button from './Button';
 
 function ColorGenerator() {
   const [backgroundColor, setBackgroundColor] = useState('#FFFFFF');
+  const colorBoxRef = useRef(null);
 
-  function generateColor() {
+  const generateColor = () => {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     setBackgroundColor(randomColor);
-  }
+    colorBoxRef.current.style.backgroundColor = randomColor;
+    const colorText = `Generated Color: ${randomColor}`;
+    const colorTextElements = colorBoxRef.current.querySelectorAll('.color-text');
+    colorTextElements.forEach((el) => {
+      el.textContent = colorText;
+    });
+  };
 
   return (
-    <div>
-      <Button onClick={generateColor} />
-      <div style={{ backgroundColor: backgroundColor }}>
-        Generated Color: {backgroundColor}
+    <div className="container">
+      <div className="color-box" ref={colorBoxRef}>
+        <div className="color-text">
+          Generated Color: <span className="color-hex">{backgroundColor}</span>
+        </div>
       </div>
+      <Button onClick={generateColor} />
     </div>
   );
 }
