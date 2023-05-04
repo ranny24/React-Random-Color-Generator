bc7aec8ba6fefca67d2163bdadc24d0872c4165c
Mona Lisa Octocat <raniamet.90@gmail.com>
2023-05-04 08:33:48 +0200

    Finale

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function ColorGenerator() {
           <span className="color-hex">{backgroundColor}</span>
         </div>
       </div>
+
       <Button onClick={generateColor} />
 
       {backgroundColor !== '#FFFFFF' && (
--- src/Colorgenerator.js
+++ src/Colorgenerator.js
@@ -25,6 +25,7 @@ function ColorGenerator() {
           Generated Color: <span className="color-hex">{backgroundColor}</span>
         </div>
       </div>
+
       <Button onClick={generateColor} />
     </div>
   );
