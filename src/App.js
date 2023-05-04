dd76019281c45a4df345e1535be6d60658ea9457
Mona Lisa Octocat <raniamet.90@gmail.com>
2023-05-04 20:13:07 +0200

    Final

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 import './App.css';
 import React, { useState } from 'react';
-import Button from './Button';
 
-// Define the ColorGenerator component
 function ColorGenerator() {
-  const [backgroundColor, setBackgroundColor] = useState('#FFFFFF');
+  const [backgroundColor, setBackgroundColor] = useState("#FFFFFF"); 
 
   const generateColor = () => {
-    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
     setBackgroundColor(randomColor);
   };
 
-  // Render the component
   return (
-    <div className="container">
-      <div className="color-box" style={{ backgroundColor: backgroundColor }}>
-        <div className="color-text">
-        Generated Color: {backgroundColor}
-          <span className="color-hex">{backgroundColor}</span>
+    <div className="container" style={{ backgroundColor: backgroundColor, backgroundImage: 'url(/images/1282786204310.jpg)', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
+      <div className="color-box">
+        <div className="generated-color" style={{ backgroundColor: backgroundColor }}>
+          Generated Color: {backgroundColor}
         </div>
+        <button className="generate-button" onClick={generateColor}>
+          Generate Color
+        </button>
       </div>
-      <Button onClick={generateColor} />
-
-      {backgroundColor !== '#FFFFFF' && (
-        <div className="generated-color">
-          {backgroundColor}
-        </div>
-      )}
     </div>
   );
 }
-
-<div> Generated Color </div>
 
 export default ColorGenerator;
